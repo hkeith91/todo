b940faeee72a9db9f544dd15c8417760b3c3e7db
Hector <hector.keith91@gmail.com>
2025-10-01 12:14:54 -0400

    Add 'past_due' and refactor 'Frequencies' to use str.  Add 'custom' to 'Frequencies'

--- models/todo_item.py
+++ models/todo_item.py
@@ -5,12 +5,13 @@ from datetime import date, time
 
 
 class Frequencies(Enum):
-    daily = 0
-    weekly = 1
-    bi_weekly = 2
-    monthly = 3
-    bi_monthly = 4
-    yearly = 5
+    daily = "D"
+    weekly = "W"
+    bi_weekly = "BW"
+    monthly = "M"
+    bi_monthly = "BM"
+    yearly = "Y"
+    custom = "C"
 
 
 class TodoItem:
@@ -25,6 +26,7 @@ class TodoItem:
         is_complete: bool = False,
         due_date: Optional[date] = None,
         due_time: Optional[time] = None,
+        past_due: bool = False,
         priority: Optional[int] = None,
         recurring: bool = False,
         frequency: Optional[int] = None,
@@ -34,6 +36,7 @@ class TodoItem:
         self.is_complete = is_complete
         self.due_date = due_date
         self.due_time = due_time
+        self.past_due = past_due
         self.priority = priority
         self.recurring = recurring
         self.frequency = frequency
