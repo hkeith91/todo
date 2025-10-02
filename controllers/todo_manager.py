e7c6aa0214a10470598c7f4894e98b504b9d1a96
Hector <hector.keith91@gmail.com>
2025-10-02 11:41:06 -0400

    Add todo list

--- controllers/todo_manager.py
+++ controllers/todo_manager.py
@@ -1,3 +1,10 @@
+# TODO: Get Todo by id
+# TODO: Add Todo item
+# TODO: Edit Todo by id
+# TODO: Delete Todo by id
+# TODO: Sort Todo's by priority
+# TODO: Sort Todo's by date_date
+# TODO: Create multiple lists
 class TodoManager:
     def __init__(self):
         self.todo_list = []
