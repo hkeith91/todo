61c8db3e3b32a9de52ae873f902a0e6e83fecc91
Hector <hector.keith91@gmail.com>
2025-10-03 21:12:43 -0400

    Implement get_todo_item_by_id

--- controllers/todo_manager.py
+++ controllers/todo_manager.py
@@ -1,13 +1,22 @@
-# TODO: Get Todo by id
 # TODO: Add Todo item
+# TODO: Get Todo by id
 # TODO: Edit Todo by id
 # TODO: Delete Todo by id
 # TODO: Sort Todo's by priority
 # TODO: Sort Todo's by date_date
 # TODO: Create multiple lists
+from models.todo_item import TodoItem
+from typing import List, Optional
+
+
 class TodoManager:
     def __init__(self):
         self.todo_list = []
 
-    def get_all_todo_items(self):
+    def get_all_todo_items(self) -> List[TodoItem]:
         return self.todo_list
+
+    def get_todo_item_by_id(self, id_to_search: str) -> Optional[TodoItem]:
+        return next(
+            (item for item in self.todo_list if item.todo_id == id_to_search), None
+        )
