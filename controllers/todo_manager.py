89e186233ca3a3d677877230c1b33b71e839759f
Hector <hector.keith91@gmail.com>
2025-10-06 11:03:56 -0400

    Add uniqueness check for todo_id before appending to list

--- controllers/todo_manager.py
+++ controllers/todo_manager.py
@@ -3,13 +3,22 @@
 # TODO: Sort Todo's by priority
 # TODO: Sort Todo's by date_date
 # TODO: Create multiple lists
-from models.todo_item import TodoItem
 from typing import List, Optional
+
+from black.comments import children_contains_fmt_on
+
+from models.todo_item import TodoItem
 
 
 class TodoManager:
     def __init__(self):
         self.todo_list = []
+
+    def check_id_is_unique(self, id_to_check: str) -> bool:
+        todo_id_set = {item.todo_id for item in self.todo_list}
+        initial_length = len(todo_id_set)
+        todo_id_set.add(id_to_check)
+        return len(todo_id_set) == initial_length
 
     def get_all_todo_items(self) -> List[TodoItem]:
         return self.todo_list
@@ -20,8 +29,14 @@ class TodoManager:
         )
 
     def add_todo_item(self, item_to_add: TodoItem) -> Optional[TodoItem]:
-        self.todo_list.append(item_to_add)
-        return item_to_add
+        if item_to_add.todo_id is None:
+            item_to_add.todo_id = TodoItem.generate_unique_id()
+
+        if self.check_id_is_unique(item_to_add.todo_id):
+            self.todo_list.append(item_to_add)
+            return item_to_add
+        else:
+            raise ValueError("Supplied todo_id already exists in Manager list")
 
     def delete_todo_item(self, id_to_delete):
         pass
