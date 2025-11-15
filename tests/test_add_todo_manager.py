f3b345486d69c193f1f77c1aeed02af8d38c5fe5
Hector <hector.keith91@gmail.com>
2025-11-15 12:12:29 -0500

    Change manager.todo_list to manager.todo_items

--- tests/test_add_todo_manager.py
+++ tests/test_add_todo_manager.py
@@ -12,7 +12,7 @@ def test_add_todo_item_appends_to_empty_list(todo_test_data: List[TodoItem]):
     item_to_add = todo_test_data[0]
     manager.add_todo_item(item_to_add)
 
-    assert len(manager.todo_list) == 1, "Failed to add item to empty list"
+    assert len(manager.todo_items) == 1, "Failed to add item to empty list"
 
 
 def test_add_todo_item_appends_correct_item_to_empty_list(
@@ -24,7 +24,7 @@ def test_add_todo_item_appends_correct_item_to_empty_list(
     manager.add_todo_item(item_to_add)
 
     assert (
-        manager.todo_list[0] == item_to_add
+        manager.todo_items[item_to_add.todo_id] == item_to_add
     ), "The tested item did not match item that was added"
 
 
@@ -33,13 +33,13 @@ def test_add_todo_item_appends_to_non_empty_list(
 ):
     """Asserts the Manager appends the item to a non-empty list"""
     manager = TodoManager()
-    manager.todo_list = deepcopy(todo_test_data)
-    initial_length = len(manager.todo_list)
+    manager.todo_items = deepcopy(todo_test_data)
+    initial_length = len(manager.todo_items)
     item_to_add = todo_test_item
     manager.add_todo_item(item_to_add)
 
     assert (
-        len(manager.todo_list) == initial_length + 1
+        len(manager.todo_items) == initial_length + 1
     ), "The number of elements in the list did not match expected value"
 
 
@@ -48,18 +48,18 @@ def test_add_todo_item_appends_correct_item_to_non_empty_list(
 ):
     """Asserts the Manager appends the correct item to a non-empty list"""
     manager = TodoManager()
-    manager.todo_list = deepcopy(todo_test_data)
+    manager.todo_items = deepcopy(todo_test_data)
     item_to_add = todo_test_item
     manager.add_todo_item(item_to_add)
 
     assert (
-        manager.todo_list[-1] == item_to_add
+        manager.todo_items[-1] == item_to_add
     ), "The tested item did not match the expected value"
 
 
 def test_add_non_unique_id_raises_exception(todo_test_data: List[TodoItem]):
     """Asserts the method will raise a ValueError when adding an ID that already exists"""
     manager = TodoManager()
-    manager.todo_list = deepcopy(todo_test_data)
+    manager.todo_items = deepcopy(todo_test_data)
     with pytest.raises(ValueError):
         manager.add_todo_item(todo_test_data[0])
