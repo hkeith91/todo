ac8c0ed6dd871c03a045ef7ef377857f49b3aa87
Hector <hector.keith91@gmail.com>
2025-11-17 04:25:30 -0500

    Refactor to use dict

--- tests/test_add_todo_manager.py
+++ tests/test_add_todo_manager.py
@@ -2,8 +2,7 @@ import pytest
 from typing import List
 from models.todo_item import TodoItem
 from models.todo_manager import TodoManager
-from copy import deepcopy
-from tests.fixtures import todo_test_item, todo_test_data
+from tests.fixtures import todo_test_item, todo_test_data, fixture_list_to_dict
 
 
 def test_add_todo_item_appends_to_empty_list(todo_test_data: List[TodoItem]):
@@ -33,7 +32,7 @@ def test_add_todo_item_appends_to_non_empty_list(
 ):
     """Asserts the Manager appends the item to a non-empty list"""
     manager = TodoManager()
-    manager.todo_items = deepcopy(todo_test_data)
+    manager.todo_items = fixture_list_to_dict(todo_test_data)
     initial_length = len(manager.todo_items)
     item_to_add = todo_test_item
     manager.add_todo_item(item_to_add)
@@ -48,18 +47,18 @@ def test_add_todo_item_appends_correct_item_to_non_empty_list(
 ):
     """Asserts the Manager appends the correct item to a non-empty list"""
     manager = TodoManager()
-    manager.todo_items = deepcopy(todo_test_data)
+    manager.todo_items = fixture_list_to_dict(todo_test_data)
     item_to_add = todo_test_item
     manager.add_todo_item(item_to_add)
 
     assert (
-        manager.todo_items[-1] == item_to_add
+        manager.todo_items[item_to_add.todo_id] == item_to_add
     ), "The tested item did not match the expected value"
 
 
 def test_add_non_unique_id_raises_exception(todo_test_data: List[TodoItem]):
     """Asserts the method will raise a ValueError when adding an ID that already exists"""
     manager = TodoManager()
-    manager.todo_items = deepcopy(todo_test_data)
+    manager.todo_items = fixture_list_to_dict(todo_test_data)
     with pytest.raises(ValueError):
         manager.add_todo_item(todo_test_data[0])
